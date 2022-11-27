f9e8455d11e4201b5ae6d463bd1b319dcbbaed8f
fja-odoo <fja@odoo.com>
2022-11-27 23:37:26 +0800

    [ADD] profitability and populate

--- training/models/training.py
+++ training/models/training.py
@@ -1,19 +1,39 @@
 #-*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from odoo.tools import populate
 
 
 class training(models.Model):
     _name = 'training.training'
     _description = 'training.training'
+    _populate_sizes = {
+        'small': 1000,
+        'medium': 100000,
+        'large': 1000000,
+    }
 
     name = fields.Char()
     value = fields.Float()
     max_attendees = fields.Integer(string="Maximum Attendees")
     expected_revenue = fields.Float(compute="_compute_expected_revenue", store=True)
+    profitability = fields.Float(compute="_compute_profitability")
     description = fields.Text()
 
     @api.depends('value', 'max_attendees')
     def _compute_expected_revenue(self):
         for record in self:
             record.expected_revenue = record.value * record.max_attendees
+
+    def _compute_profitability(self):
+        for record in self:
+            trainings = self.search([])
+            average = sum(trainings.mapped('expected_revenue')) / len(trainings)
+            record.profitability =  average / record.expected_revenue if record.expected_revenue else 0
+
+    def _populate_factories(self):
+        return [
+            ("name", populate.constant('Training_{counter}')),
+            ("value", populate.randfloat(0, 100)),
+            ("max_attendees", populate.randfloat(1, 50)),
+        ]
